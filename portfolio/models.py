73aa9363d39470d53e4bcbeb9bab447ec77058d4
wallyHack <m.herreralara80@gmail.com>
2020-04-04 20:25:53 -0600

    se añadieron enlaces a los proyectos

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Proyecto(models.Model):
     titulo = models.CharField(blank=False, null=False, max_length=100) # cadena corta
     descripcion = models.TextField(blank=True, null=False) # cadena larga
     imagen = models.ImageField(upload_to="proyectos/", null=True)
+    link = models.URLField(blank=True, null=True, verbose_name='Dirección Web')
     # se añade la hora automaticamente cuando se cree por primera vez(solo al crearse)
     fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación') # verbose_name -> es como se muestra en al admin site 
     # cada vez que se modifica
